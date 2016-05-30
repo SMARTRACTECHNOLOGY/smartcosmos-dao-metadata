a41d1cf9baa15dfbc3a0adad2297aed4e5386c4b
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-30 10:10:47 +0200

    Add Builder annotation to MetadataCreate

--- src/main/java/net/smartcosmos/dto/metadata/MetadataCreate.java
+++ src/main/java/net/smartcosmos/dto/metadata/MetadataCreate.java
@@ -1,9 +1,11 @@
 package net.smartcosmos.dto.metadata;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MetadataCreate {
     private String dataType;
