821664033fc8942d070f074459896dcb4758f48c
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-03 10:50:59 +0200

    Add version to DTOs including tests

---
+++ src/test/java/net/smartcosmos/dto/metadata/MetadataResponseTest.java
@@ -0,0 +1,36 @@
+package net.smartcosmos.dto.metadata;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class MetadataResponseTest {
+
+    @Test
+    public void thatVersionIsSet() {
+        MetadataResponse entity = MetadataResponse.builder().build();
+
+        assertNotNull(entity.getVersion());
+        assertEquals(1, entity.getVersion());
+    }
+
+    /**
+     * This actually tests if Lombok is properly used.
+     */
+    @Test
+    public void thatVersionHasNoSetter() {
+        Method getVersion = null;
+        try {
+            getVersion = MetadataResponse.class.getDeclaredMethod("setVersion", int.class);
+        } catch (NoSuchMethodException e) {
+            // that's what we expect
+        }
+        assertNull(getVersion);
+    }
+
+
+}
