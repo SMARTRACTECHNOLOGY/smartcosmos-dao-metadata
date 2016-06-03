4e0fbfb5572cf382c5cc77f18ea030fa5b767e51
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-03 13:36:20 +0200

    Fix countBySearchCriteria signature

--- src/main/java/net/smartcosmos/dao/metadata/MetadataDao.java
+++ src/main/java/net/smartcosmos/dao/metadata/MetadataDao.java
@@ -46,22 +46,23 @@ public interface MetadataDao {
     Optional<MetadataResponse> findByKey(String accountUrn, String entityReferenceType, String referenceUrn, String key);
 
     /**
-     * Finds a list of associated entities of a specified entity reference type within a given account that match specified metadata search criterias.
+     * Finds a list of associated entities of a specified entity reference type within a given account that match specified metadata search criteria.
      *
      * @param accountUrn the account URN
      * @param entityReferenceType the reference type of the associated entity
-     * @param queryMetadataCollection the collection of search criterias
+     * @param queryMetadataCollection the collection of search criteria
      * @param count the maximum count of results to return
      * @return a list of {@link MetadataQueryMatchResponse} instances of reference entities
      */
     Set<MetadataQueryMatchResponse> findBySearchCriteria(String accountUrn, String entityReferenceType, Collection<MetadataQuery> queryMetadataCollection, Integer count);
 
     /**
-     * Counts the metadata entities of a given account that match specified search criterias.
+     * Counts the associated entities of a specified entity reference type within a given account that match specified metadata search criteria.
      *
      * @param accountUrn the account URN
-     * @param queryMetadataCollection the collection of search criterias
+     * @param entityReferenceType the reference type of the associated entity
+     * @param queryMetadataCollection the collection of search criteria
      * @return the count of matching metadata entities
      */
-    Long countBySearchCriteria(String accountUrn, Collection<MetadataQuery> queryMetadataCollection);
+    Long countBySearchCriteria(String accountUrn, String entityReferenceType, Collection<MetadataQuery> queryMetadataCollection);
 }
