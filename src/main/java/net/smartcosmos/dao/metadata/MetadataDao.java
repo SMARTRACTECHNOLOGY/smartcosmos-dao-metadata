999bcabdb24d827ae218fbcf35ecb8195ca19e7f
Jan Bauer <jan.bauer@smartrac-group.com>
2016-06-16 11:01:18 +0200

    changed update, removed query

--- src/main/java/net/smartcosmos/dao/metadata/MetadataDao.java
+++ src/main/java/net/smartcosmos/dao/metadata/MetadataDao.java
@@ -1,10 +1,7 @@
 package net.smartcosmos.dao.metadata;
 
-import net.smartcosmos.dto.metadata.MetadataQuery;
-import net.smartcosmos.dto.metadata.MetadataQueryMatchResponse;
 import net.smartcosmos.dto.metadata.MetadataResponse;
 import net.smartcosmos.dto.metadata.MetadataCreate;
-import net.smartcosmos.dto.metadata.MetadataUpdate;
 
 import javax.validation.ConstraintViolationException;
 import java.util.Collection;
@@ -17,7 +14,7 @@ public interface MetadataDao {
     /**
      * Inserts a list of metadata entities associated to a reference entity in the realm of a given tenant.
      *
-     * @param tenantId the tenant URN
+     * @param tenantId the tenant ID
      * @param createMetadataCollection the collection containing the metadata entries to insert
      * @return an {@link MetadataResponse} list for the inserted metadata
      * @throws ConstraintViolationException if the {@link MetadataCreate} violates constraints enforced by the persistence service
@@ -26,22 +23,30 @@ public interface MetadataDao {
             throws ConstraintViolationException;
 
     /**
-     * Updates a list of metadata entities associated to a reference entity in the realm of a given tenant.
+     * Updates a metadata key associated to a reference entity with a new value in the realm of a given tenant.
      *
-     * @param tenantId the tenant URN
-     * @param updateMetadataCollection the collection containing the metadata entries to update
-     * @return an {@link MetadataResponse} list for the updated metadata
-     * @throws ConstraintViolationException if the {@link MetadataUpdate} violates constraints enforced by the persistence service
+     * @param tenantId the tenant ID
+     * @param ownerType the reference type of the associated entity
+     * @param ownerId the ID of the associated entity
+     * @param keyName the keyName of the metadata entity
+     * @param value the new value of the metadata entity
+     * @throws ConstraintViolationException if value violates constraints or does not match the dataType given on create
+     * @return
      */
-    List<MetadataResponse> update(String tenantId, Collection<MetadataUpdate> updateMetadataCollection)
+    Optional<MetadataResponse> update(
+            String tenantId,
+            String ownerType,
+            String ownerId,
+            String keyName,
+            Object value)
             throws ConstraintViolationException;
 
     /**
      * Deletes an existing metadata keyName that is associated to an entity (identified by its reference URN keyName).
      *
-     * @param tenantId the tenant URN
+     * @param tenantId the tenant ID
      * @param ownerType the reference type of the associated entity
-     * @param ownerId the URN of the associated entity
+     * @param ownerId the ID of the associated entity
      * @param keyName the keyName of the metadata entity
      * @return a list of {@link MetadataResponse} instances for the deleted metadata entities
      */
@@ -50,7 +55,7 @@ public interface MetadataDao {
     /**
      * Finds a metadata entity for an associated entity matching a specified keyName in the realm of a given tenant.
      *
-     * @param tenantId the tenant URN
+     * @param tenantId the tenant ID
      * @param ownerType the reference type of the associated entities
      * @param ownerId the URN of the associated entity
      * @param keyName the keyName of the metadata entity
@@ -61,38 +66,11 @@ public interface MetadataDao {
     /**
      * Find all metadata owned by a thing in the realm of a given tenant.
      *
-     * @param tenantId the tenant URN
+     * @param tenantId the tenant ID
      * @param ownerType the reference type of the associated entities
      * @param ownerId the URN of the associated entity
      * @param keyNames return metadata with given keys, or all metadata when keyNames is Empty
      * @return
      */
     Set<MetadataResponse> findByOwner(String tenantId, String ownerType, String ownerId, Collection<String>keyNames);
-
-    /**
-     * Finds a list of associated entities of a specified entity reference type within a given tenant that match specified metadata search criteria.
-     *
-     * @param tenantId the tenant URN
-     * @param ownerType the reference type of the associated entity
-     * @param queryMetadataCollection the collection of search criteria
-     * @param page the number of the results page
-     * @param size the size of a results page
-     * @return a list of {@link MetadataQueryMatchResponse} instances of reference entities
-     */
-    Set<MetadataQueryMatchResponse> findBySearchCriteria(
-        String tenantId,
-        String ownerType,
-        Collection<MetadataQuery> queryMetadataCollection,
-        Long page,
-        Long size);
-
-    /**
-     * Counts the associated entities of a specified entity reference type within a given tenant that match specified metadata search criteria.
-     *
-     * @param tenantId the tenant URN
-     * @param ownerType the reference type of the associated entity
-     * @param queryMetadataCollection the collection of search criteria
-     * @return the count of matching metadata entities
-     */
-    Long countBySearchCriteria(String tenantId, String ownerType, Collection<MetadataQuery> queryMetadataCollection);
 }
