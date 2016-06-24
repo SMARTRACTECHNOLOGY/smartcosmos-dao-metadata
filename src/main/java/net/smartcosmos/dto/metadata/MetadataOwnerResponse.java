4dfeef3efd4cc103b90515e6d45028a55c926956
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-24 15:13:56 +0200

    Add owner response model

--- src/main/java/net/smartcosmos/dto/metadata/MetadataOwnerResponse.java
+++ src/main/java/net/smartcosmos/dto/metadata/MetadataOwnerResponse.java
@@ -1,7 +1,21 @@
 package net.smartcosmos.dto.metadata;
 
-/**
- * Created by asiegel on 24/06/16.
- */
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonIgnoreProperties({"version"})
 public class MetadataOwnerResponse {
+
+    private static final int VERSION = 1;
+    private final int version = VERSION;
+
+    private final String ownerType;
+
+    private final String ownerUrn;
+
+    private final String tenantUrn;
 }
